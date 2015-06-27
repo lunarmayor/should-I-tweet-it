0dfb8da10dfb32e26c3d44e7258933246964c7bd
Michael Morrissey <mikehmorrissey@gmail.com>
2015-06-27 17:55:19 -0400

    create PulseMenu Component

---
+++ react_components/PulseMenu.js
@@ -0,0 +1,25 @@
+import { React } from 'react'
+
+class PulseMenu extends React.Component {
+  toggleDrawer() {
+    React.findDOMNode(this.refs.drawer)
+      .classList.toggle('is-open');
+  }
+
+  render() {
+    return (
+      <div className="pulseMenu">
+        <div className="pulseMenu-orb"></div>
+        <div className="pulseMenu-pulse" onClick={this.toggleDrawer}></div>
+        <div className="pulseMenu-drawerContainer">
+          <div className="pulseMenu-drawer" ref="drawer">
+            <img className="pulseMenu-lunarLogo" src="http://i59.tinypic.com/2ch2gw6.png"/>
+            <div className="pulseMenu-lunarText">Lunar<br/>Mayor</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PulseMenu
