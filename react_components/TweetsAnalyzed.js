e3418fee36e06fba3072eb6c56844001aaaf775e
Michael Morrissey <mikehmorrissey@gmail.com>
2015-06-27 17:55:56 -0400

    create TweetsAnalyzed component

---
+++ react_components/TweetsAnalyzed.js
@@ -0,0 +1,36 @@
+import { React } from 'react'
+
+// <TweetsAnalyzed >
+class TweetsAnalyzed extends React.Component {
+  getInitialState() {
+    return {
+      tweetsAnalyzed: 12404204,
+    }
+  }
+
+  componentDidMount() {
+    setInterval(this.incrementTweets, 300)
+  }
+
+  incrementTweets() {
+    let randomInc = Math.ceil(Math.random() * 4);
+    this.setState({
+      tweetsAnalyzed: this.state.tweetsAnalyzed + randomInc,
+    });
+  }
+
+  render() {
+    return (
+      <div className="tweetAnalyzer">
+        <div className="tweetAnalyzer-count">
+          {this.state.tweetsAnalyzed.toLocaleString()}
+        </div>
+        <div className="tweetAnalyzer-desc">
+          Tweets Analyzed
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TweetsAnalyzed
