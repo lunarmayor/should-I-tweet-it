3a05cd5561f8879ea851478d04bd13acc788955f
Michael Morrissey <mikehmorrissey@gmail.com>
2015-07-22 15:52:26 -0400

    add analyze tweet/ transition

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,12 +1,16 @@
 import Backbone from 'backbone'
+import _ from 'underscore'
 import Router from './Router'
 import Ready from './utilities/Ready'
+import extend from './utilities/extend'
 
-let ShouldITweetIt = {
+window.ShouldITweetIt = {
   start: function() {
     new Router();
     Backbone.history.start({ pushState: true })
   }
 }
+
+_.extend(ShouldITweetIt, Backbone.Events)
 
 Ready( () => { ShouldITweetIt.start() })
--- assets/javascripts/router.js
+++ assets/javascripts/router.js
@@ -1,21 +1,47 @@
 import App from '../../es6/react_components/App'
 import Backbone from 'backbone'
 import DOM from './utilities/DOM'
+import { Promise } from 'es6-promise'
 import React from 'react'
+import TweetAnalyzer from '../../es6/react_components/TweetAnalyzer'
+import Loader from '../../es6/react_components/GeometricLoader'
 
 class Router extends Backbone.Router {
   routes() {
     return {
-      '': 'setupApp'
+      '': 'setupApp',
+      'analyze': 'analyzeTweet'
     }
   }
 
   constructor(options) {
     super()
+    ShouldITweetIt.on('analyzeTweet', (text) => this.analyzeTweet(text))
   }
 
   setupApp() {
     React.render(<App />, DOM.find('.app-container'));
+  }
+
+  analyzeTweet(text) {
+    new Promise( (resolve) => this.hideMainContainer(resolve) )
+      .then( () => this.showLoader() )
+  }
+
+  hideMainContainer(resolve) {
+    let mainContainer = DOM.find('.main-innerContainer')
+    let main = DOM.find('main')
+    main.addEventListener('transitionend', () => resolve() )
+    mainContainer.classList.add('is-hidden')
+    main.classList.add('is-hidden')
+  }
+
+  showLoader() {
+    React.render(<GeometricLoader/>, DOM.find('.main'))
+  }
+
+  showTweetAnalyzer() {
+    React.render(<TweetAnalyzer text={ text }/>, DOM.find('.main'));
   }
 }
 
