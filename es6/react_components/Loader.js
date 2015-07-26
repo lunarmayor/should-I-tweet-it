5c58145d5d0282cc358aa6099191bd1df7c32951
Michael Morrissey <mikehmorrissey@gmail.com>
2015-07-26 17:35:41 -0400

    create loader

--- es6/react_components/Loader.js
+++ es6/react_components/Loader.js
@@ -1,9 +1,43 @@
 import React from 'react'
 import THREE from 'three'
 import IcosahedronLoader from './IcosahedronLoader'
+import CanNavigate from './../decorators/CanNavigate'
+
+let loadingMessages = [
+  'priming semantic engines',
+  'chanting big data',
+  'praying to the almighty Jobs',
+  'crawling the social graph',
+  'introspecting on my choices',
+]
 
 // <Loader />
+@CanNavigate
 class Loader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { descNum: 0 }
+  }
+
+  componentDidMount() {
+    this.timer = setInterval( () => this.changeMessage(), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  changeMessage() {
+    if(this.state.descNum > 3) {
+      clearInterval(this.timer)
+      this.transitionTo('analyzing_tweet')
+    } else {
+      this.setState({
+        descNumber: this.state.descNum++,
+      })
+    }
+  }
+
   render() {
     return (
       <div className="loader">
@@ -13,7 +47,7 @@ class Loader extends React.Component {
             <div className="loader-pulse2"></div>
             <IcosahedronLoader width="200" height="200"/>
           </div>
-          <div className="loader-desc">priming semantic engines</div>
+          <div className="loader-desc">{loadingMessages[this.state.descNum]}</div>
         </div>
       </div>
     )
