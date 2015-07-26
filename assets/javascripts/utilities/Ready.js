6a7fb5f15586537274bb3d59ca76ff5f16c22cb3
Michael Morrissey <mikehmorrissey@gmail.com>
2015-07-26 00:35:27 -0400

    move DOM lib to private npm module

--- assets/javascripts/utilities/Ready.js
+++
@@ -1,9 +0,0 @@
-function ready(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-export default ready
--- assets/javascripts/utilities/extend.js
+++
@@ -1,9 +0,0 @@
-function extend (target, source) {
-    var a = Object.create(target);
-    Object.keys(source).map(function (prop) {
-        prop in a && (a[prop] = source[prop]);
-    });
-    return a;
-};
-
-export default extend
