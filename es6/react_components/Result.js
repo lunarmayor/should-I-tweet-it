76144bdae04886f60ab1423b1680ad9255d74cb5
Michael Morrissey <mikehmorrissey@gmail.com>
2015-07-30 00:42:35 -0400

    add tweet it anyway

--- es6/react_components/Result.js
+++ es6/react_components/Result.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import TweetStore from './../stores/TweetStore'
 
 class Result extends React.Component {
   componentDidMount() {
-    React.findDOMNode(this.refs.no).classList.add('is-growing')
+    React.findDOMNode(this.refs.no).classList.add('is-growing');
+    React.findDOMNode(this.refs.tweetLink).classList.add('is-entering');
   }
 
   render() {
     return (
       <div className="result">
         <div className="result-should">Should I tweet it?</div>
-        <div className="result-no" ref="no">NO</div>
+        <div className="result-no" ref="no">
+          <div>NO</div>
+          <a ref="tweetLink" className="result-tweet"
+             href={`https://twitter.com/intent/tweet?text=${TweetStore.text}`}>
+             {'tweet it anyway :)'}
+          </a>
+        </div>
       </div>
     )
   }
--- es6/react_components/SideNav.js
+++ es6/react_components/SideNav.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import IcosahedronLogo from './IcosahedronLogo'
+import { Link } from 'react-router'
 
 // <SideNav />
 class SideNav extends React.Component {
   render() {
     return (
       <nav className="sideNav">
-        <IcosahedronLogo width="80" height="80" />
+        <Link to='/'>
+          <IcosahedronLogo width="80" height="80" />
+        </Link>
       </nav>
     );
   }
